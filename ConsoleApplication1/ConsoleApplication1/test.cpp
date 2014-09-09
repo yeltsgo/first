0956a4f21b5b6a2b500f560d7c0954e6bc27bd0b
gs600981 <gs600981@gmail.com>
2014-09-09 14:21:16 +0800

    摄像头捕获

--- ConsoleApplication1/ConsoleApplication1/test.cpp
+++ ConsoleApplication1/ConsoleApplication1/test.cpp
@@ -3,11 +3,25 @@
 using namespace cv;
 void main()
 {
-	IplImage *src;
-	src = cvLoadImage("lena.jpg"); //这里将lena.jpg和lena.cpp文件放在同一个文件夹下
-	cvNamedWindow("lena",CV_WINDOW_AUTOSIZE);
-	cvShowImage("lena",src);
-	cvWaitKey(0);
-	cvDestroyWindow("lena");
-	cvReleaseImage(&src);
+	//获取第一个摄像头
+	CvCapture* capture;
+	capture=cvCreateCameraCapture(0);
+	assert(capture!=NULL);
+	
+	IplImage* frame;//待载入图像
+	cvNamedWindow("camera",1);//创建名称为camera的窗口。
+	while(1)
+	{
+		
+		frame=cvQueryFrame(capture);//从摄像头中抓取并返回一帧
+		if(!frame)
+			break;
+		cvShowImage("camera",frame);//在camera的窗口中显示frame图像
+		
+		if(cvWaitKey(32)>=0)
+		   break;
+	}
+	cvReleaseImage(&frame);//释放刚才载入图像的内存空间
+	cvDestroyWindow("camera");//用来销毁名为camera的窗口。
+
 }
